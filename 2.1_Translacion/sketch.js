3b208469b041a81e06f34dd0f2e02928398d4dfb
Jesus-Emilio-Ramirez-Lopez <jerlx731@gmail.com>
2026-03-04 17:41:14 +0000

    P

--- 2.1_Translacion/sketch.js
+++ 2.1_Translacion/sketch.js
@@ -7,7 +7,7 @@ function draw() {
  background(240);
  tx += 2;
  push();
- translate(tx, 0); // mueve TODO lo que dibujes dentro del push/pop
+ translate(tx, 100); // mueve TODO lo que dibujes dentro del push/pop
  fill(200, 80, 80);
  rect(150, 150, 120, 60);
  fill(80, 120, 200);
